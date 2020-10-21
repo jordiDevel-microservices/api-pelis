0aa0c39ef4faa32db3eb8af13c868dfc2cb1f1e0
Jordi Medina <jordi.medinac@gmail.com>
2020-10-21 10:17:32 +0200

    First commit

---
+++ src/main/java/org/myself/education/ApiPelisApplication.java
@@ -0,0 +1,13 @@
+package org.myself.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPelisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPelisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/myself/education/ApiPelisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myself.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPelisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
